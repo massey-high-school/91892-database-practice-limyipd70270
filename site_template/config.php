3e54525e0fb6ab959f7e04b2d85e02cc54fc0437
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-08-11 12:20:09 +1200

    just made showall.php

--- site_template/config.php
+++ site_template/config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'On');  // change to 'Off' when live
 
-define("DB_NAME","91892-database-practice-limyipd70270");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","");
+define("DB_NAME","limyipd70270");
+define("DB_USERNAME","_limyipd70270");
+define("DB_PASSWORD","a566feb4426ede7cb0bda5c8c23b2f4c");
 define("DB_HOST","localhost");
 
 ?>
