aae78b899bf98bba37792dae85feaa4c2d10c512
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-09-01 12:20:06 +1200

    result parts 1 vid

--- site_template/showall.php
+++ site_template/showall.php
@@ -1,13 +1,54 @@
   <?php include("topbit.php") ?>
 
+    $find_sql = "SELECT * FROM `L2_prac_game_details`";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
+
         <div class="box main">
             <h2>All Results</h2>
             
             
-            <p>Results will go here...
-            </p>
+            <?php
+    
+            if($count < 1) {
+                
+              ?>
             
+            <div class="error">
+            
+                Sorry! There are no results that match your search.
+                Please use the search box in the side bar to try again.
+            
+            </div>  <!-- end error -->
+          
+            <?php
+            } // end no results if
 
+            else {
+                do
+                }
+
+                    ?>
+            
+            <!-- Results go here -->
+            <div class="results">
+                You have results! 
+            </div> <!-- / results -->
+            
+            <?php
+            
+                } // end results 'do'
+            
+            
+            
+            } // end else
+            
+            ?>
+
+            
             
         </div> <!-- / main -->
 
