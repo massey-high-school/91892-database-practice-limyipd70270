fcefc04af36fd56f659fb34750168f98d3feadd9
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-09-09 10:49:46 +1200

    now shows star rating, same vid

--- site_template/showall.php
+++ site_template/showall.php
@@ -77,7 +77,7 @@
                     
                     <!-- Partial Stars Original Source: https://codepen.io/Bluetidepro/pen/GkpEa -->
                     <div class="star-ratings-sprite">
-                        <span style="width:<?php echo $find_rs['User Rating'] / 5 * 100; ?> %" class="star-ratings-sprite-rating"></span>
+                        <span style="width:<?php echo $find_rs['User Rating'] / 5 * 100; ?>%" class="star-ratings-sprite-rating"></span>
 
                     </div> <!-- / star rating div -->
 
