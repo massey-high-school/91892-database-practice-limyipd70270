96e6d53e19b77441bbc7daa55b7d1f29bcdc0276
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-09-16 10:55:15 +1200

    basic search part 3 1:10

--- site_template/name_dev.php
+++ site_template/name_dev.php
@@ -1,8 +1,11 @@
   <?php include("topbit.php");
 
+    $name_dev = $_POST['dev_name'];
+
     $find_sql = "SELECT * FROM `L2_prac_game_details` 
     JOIN `L2_prac_genre` ON (`L2_prac_game_details`.`Genre ID` = `L2_prac_genre`.`Genre ID`)
     JOIN `L2_prac_developer` on (`L2_prac_game_details`.`Developer ID` = `L2_prac_developer`.`Developer ID`)
+    WHERE `Name` LIKE '%$name_dev%' OR 'DevName' LIKE '%$name_dev%'
     ";
     $find_query = mysqli_query($dbconnect, $find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
@@ -11,7 +14,7 @@
 ?>
 
         <div class="box main">
-            <h2>All Results</h2>
+            <h2>Name / Developer Results</h2>
             
             <?php
             include ("results.php")
