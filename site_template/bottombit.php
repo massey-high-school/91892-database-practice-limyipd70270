5679573d0bf1d856819a2ac5afae1bda449e2c3b
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-09-18 14:50:14 +1200

    adv search part 2

--- site_template/bottombit.php
+++ site_template/bottombit.php
@@ -23,8 +23,45 @@
             
             <div class="advanced-frame">
             
-            <h2></h2>    
+            <h2>Advcanced Search</h2>  
+            
+            <form class="searchform" method="post" action="advanced.php" enctype="multipart/form-data">
+                
+            <input class="adv" type="text" name="app_name" size="40" value="" placeholder="App Name / Title..."/>
+                
+            <input class="adv" type="text" name="dev_name" size="40" value="" placeholder="Developer..."/>
+                
+            <!-- Genre Dropdown -->
+                
+            <select class="search adv" name="genre">
+                
+            <option value="" disabled selected>Genre...</option>
+                
+            <!-- get options from database -->
+            <?php
+                $genre_sql="SELECT * FROM `L2_prac_genre` ORDER BY `L2_prac_genre`.`Genre Name` ASC";
+                $genre_query=mysqli_query($dbconnect, $genre_sql);
+                $genre_rs = mysqli_fetch_assoc($genre_query);
+                
+                do{
+                   ?>
+                <option value="<?php echo $genre_rs['Genre Name']; ?>"><?php echo $genre_rs['Genre Name']; ?></option>
+                
+                <?php
+                    
+                }  // end genre do loop
+                
+                while ($genre_rs=mysqli_fetch_assoc($genre_query))
+                    
+            ?>
+                
+            </select>
+                
+            <input class="submit advanced-button" type="submit" name="advanced" value="Search &nbsp; &#xf002;" />
 
+            </form>
+                
+            </div>  <!-- / advanced frame -->
             
         </div> <!-- / side bar -->
 
